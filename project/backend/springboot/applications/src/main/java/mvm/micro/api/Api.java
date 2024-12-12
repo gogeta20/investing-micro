f3f89a17d707e178e8e82c2138af08009728a0a9
mau vargas <linuxlite20@gmail.com>
2024-12-12 17:40:35 +0100

    feat: health controller success

--- project/backend/springboot/applications/src/main/java/mvm/micro/api/Api.java
+++ project/backend/springboot/applications/src/main/java/mvm/micro/api/Api.java
@@ -2,8 +2,13 @@ package mvm.micro.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+    "mvm.micro.api",
+    "mvm.micro.core.infrastructure.controller"
+})
 public class Api {
     public static void main(String[] args) {
         SpringApplication.run(Api.class, args);

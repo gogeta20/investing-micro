bb4c7dcac7e0fdb72f3807b65e9b02f1813b117e
mau vargas <linuxlite20@gmail.com>
2025-01-24 09:36:45 +0100

    style: caption

--- project/front/src/core/layout/components/Header.vue
+++ project/front/src/core/layout/components/Header.vue
@@ -1,39 +1,18 @@
 <script setup lang="ts">
-import { ref, watch } from "vue";
-
 const emit = defineEmits(["menu-toggle"]);
 function onMenuToggle(event: Event) {
   event.preventDefault();
   emit("menu-toggle", event);
   event.stopPropagation();
 }
-
-
 </script>
 
 <template>
-  <header
-    :class="[
-      'fixed top-0 left-0 w-full z-50 transition-all duration-300',
-    ]"
-  >
+  <header :class="['fixed top-0 left-0 w-full z-50 transition-all duration-300']">
     <div class="container mx-auto px-4 flex items-center justify-between py-8">
-      <!-- Logo -->
-      <div class="text-2xl font-bold">
-        <a href="/" class="text-white">MV</a>
-      </div>
+      <router-link to="/" class="text-white text-2xl">MV</router-link>
 
-      <!-- Navigation -->
-      <!-- <nav class="hidden md:flex space-x-6">
-        <a href="#home" class="text-white hover:text-gray-700">Home</a>
-        <a href="#contact" class="text-white hover:text-gray-700">Contact</a>
-      </nav> -->
-
-      <!-- Mobile Menu Button -->
-      <button
-        class="text-white focus:outline-none"
-         @click="onMenuToggle"
-      >
+      <button class="text-white focus:outline-none" @click="onMenuToggle">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -53,8 +32,7 @@ function onMenuToggle(event: Event) {
   </header>
 </template>
 
-<style scoped lang='scss'>
-/* Ajustes adicionales para mejorar el efecto */
+<style scoped lang="scss">
 .bg-transparent {
   backdrop-filter: blur(10px);
 }

cc402a2b15f79f913836e4b3e54d4b89460aff56
mau vargas <linuxlite20@gmail.com>
2025-02-03 12:15:50 +0100

    feat: cloud aws y google modificacion division

---
+++ project/backend/django/myproject/core/infrastructure/controllers/logs/logs_controller.py
@@ -0,0 +1,27 @@
+from django.http import JsonResponse
+
+from myproject.core.application.UseCase.Logs.Logs import Logs
+from myproject.core.application.UseCase.Logs.LogsQuery import LogsQuery
+from myproject.core.application.UseCase.Logs.LogsQueryHandler import LogsQueryHandler
+from myproject.shared.application.handlers.handlers import get_handler
+from myproject.shared.infrastructure.bus.query_bus import QueryBus
+from myproject.shared.infrastructure.controller.api_controller import ApiController
+
+
+class LogsController(ApiController):
+    def __init__(self, query_bus: QueryBus = None):
+        query_bus = query_bus or QueryBus()
+        use_case = Logs()
+        query_bus.register(LogsQuery, LogsQueryHandler(use_case))
+        super().__init__(query_bus=query_bus)
+
+    def get(self, request, text):
+        query = LogsQuery(text)
+        response = self.ask(query)
+
+        return JsonResponse(response)
+
+    def register_exceptions(self) -> dict:
+        return {
+            Exception: 500,
+        }

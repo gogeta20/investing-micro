5b62c3cbbc5ea6b0651fabe40550440bfe6280e8
mau vargas <linuxlite20@gmail.com>
2025-02-03 09:15:43 +0100

    feat: cloud aws y google modificacion division

--- project/backend/django/myproject/settings.py
+++ project/backend/django/myproject/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+l-+%=1!1pri1*b&6z)s3&ol+&u#gz4_zawu(^ty1kw7#z_81o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", "django-aws.duckdns.org", "13.61.198.98"]
 
 # Application definition
 

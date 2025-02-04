079cd3c9e63835f7d0936b953072174c24789397
mau vargas <linuxlite20@gmail.com>
2025-02-04 16:51:44 +0100

    feat: cloud aws y google modificacion division

--- project/consumer/src/consumer/infrastructure/rabbit/consumer.rs
+++ project/consumer/src/consumer/infrastructure/rabbit/consumer.rs
@@ -20,7 +20,8 @@ impl RabbitConsumer {
     pub async fn new(queue_name: &str) -> Result<Self, lapin::Error>
     {
         let amqp_addr = config::get_amqp_addr();
-        let conn = Connection::connect(&amqp_addr, ConnectionProperties::default()).await?;
+        // let conn = Connection::connect(&amqp_addr, ConnectionProperties::default()).await?;
+        let conn = Connection::connect("amqp://admin:your_password@34.45.214.187:5672/%2f/messages", ConnectionProperties::default()).await?;
         let channel = conn.create_channel().await?;
 
         Ok(Self {

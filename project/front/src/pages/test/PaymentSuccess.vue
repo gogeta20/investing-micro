0e7e5654ffe06c2165c1ffac48075d02cd084f4f
mau vargas <linuxlite20@gmail.com>
2025-03-31 11:56:24 +0200

    feat: backend primer test de confirmacion de pago paypal

--- project/front/src/pages/test/PaymentSuccess.vue
+++ project/front/src/pages/test/PaymentSuccess.vue
@@ -7,11 +7,7 @@ import { useRoute } from 'vue-router';
 const route = useRoute();
 
 onMounted(async () => {
-  const token = route.query.token;
-  // const tokenSend = token == null ? '' : token;
-  // Aquí puedes hacer la llamada al backend con el token
-  // para confirmar el pago si no se ha hecho todavía
-  console.log('Confirmando pago con token:', token);
+  const token = route.query.token as string;
   const useCase = new GetConfirmPaymentPaypal(new RepositorySymfonyGet());
   const response = await useCase.execute(token);
   console.log(response)
@@ -19,8 +15,10 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="text-center p-8">
-    <h1 class="text-2xl text-green-500 font-bold mb-4">✅ ¡Pago confirmado!</h1>
-    <p>Gracias por tu compra. Te enviaremos un resumen a tu correo electrónico.</p>
+  <div class="bg-green page-container">
+    <div class="text-center p-8">
+      <h1 class="text-2xl text-green-500 font-bold mb-4">✅ ¡Pago confirmado!</h1>
+      <p class="text-gray-300">Gracias por tu compra. Te enviaremos un resumen a tu correo electrónico.</p>
+    </div>
   </div>
 </template>

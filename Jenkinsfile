dd271db3e00c6d1a31934d123744c6c7f11b2ae4
mau vargas <linuxlite20@gmail.com>
2025-04-28 10:14:40 +0200

    feat: ci whit dockerfiles of test

--- Jenkinsfile
+++ Jenkinsfile
@@ -17,7 +17,7 @@ pipeline {
                     sonar-scanner \
                     -Dsonar.projectKey=symfony_project \
                     -Dsonar.sources=./project/backend/symfony \
-                    -Dsonar.host.url=http://sonar:9000 \
+                    -Dsonar.host.url=http://sonar:9100 \
                     -Dsonar.login=squ_077ab16d273236b5ce68c2a830e72efcd7f48c47 \
                     '''
                 }
--- devops/mk/ci.mk
+++ devops/mk/ci.mk
@@ -10,13 +10,13 @@ up-ci:
 	docker compose -f docker-compose.extra.yml --profile ci up -d
 
 up-ci-test:
-	docker compose -f docker-compose.test.yml --profile ci up -d
+	docker compose -f docker-compose.test.yml --profile ci-main up -d
 
 build-ci-test:
-	docker compose -f docker-compose.test.yml --profile ci build
+	docker compose -f docker-compose.test.yml --profile ci-main build
 
 down-ci-test:
-	docker compose -f docker-compose.test.yml --profile ci down
+	docker compose -f docker-compose.test.yml --profile ci-main down
 
 sonar-space:
 	sudo sysctl -w vm.max_map_count=262144

a6778347f3435d31db0fce6a023a7e5eaa2c5f5b
mau vargas <linuxlite20@gmail.com>
2025-05-01 09:49:04 +0200

    feat: Finish use case OpenAI

--- project/backend/fastapi/app/application/handle_message.py
+++ project/backend/fastapi/app/application/handle_message.py
@@ -1,3 +1,4 @@
+from app.application.use_cases.ask_ai_use_case import AskAIUseCase
 from app.shared.models import ChatRequest, ChatResponse
 from app.application.use_cases.greet_user import greet_user_use_case
 from app.infrastructure.ml.intent_classifier import IntentClassifierService
@@ -9,6 +10,10 @@ async def handle_message(payload: ChatRequest) -> ChatResponse:
     intent, confidence = classifier.predict(payload.message)
 
     confidence_threshold = 0.6
+
+    if confidence < 0.6:
+        return await AskAIUseCase.execute(payload.message)
+
     if confidence < confidence_threshold:
         return ChatResponse(
             intent="desconocido",
--- project/backend/fastapi/app/application/use_cases/ask_ai_use_case.py
+++ project/backend/fastapi/app/application/use_cases/ask_ai_use_case.py
@@ -8,13 +8,24 @@ class AskAIUseCase:
     @staticmethod
     async def execute(message: str) -> ChatResponse:
         try:
-            response = openai.ChatCompletion.create(
+
+            client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+            response = client.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=[
-                    {"role": "system", "content": "Responde de forma clara, concisa y útil como un asistente."},
-                    {"role": "user", "content": message}
+                    {"role": "user", "content": "¿Qué modelo eres?"}
                 ]
             )
+
+            # response = openai.ChatCompletion.create(
+            # response =  client.chat.completions.create(
+            #     model="gpt-3.5-turbo",
+            #     messages=[
+            #         {"role": "system", "content": "Responde de forma clara, concisa y útil como un asistente."},
+            #         {"role": "user", "content": message}
+            #     ]
+            # )
             text = response.choices[0].message["content"]
             return ChatResponse(
                 intent="respuesta_ia",

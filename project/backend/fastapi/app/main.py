66b03670d7f50f2357665d14bea39a0d2f76e59b
mau vargas <linuxlite20@gmail.com>
2025-05-08 18:14:28 +0200

    feat: add prometheus service metrics

--- project/backend/fastapi/app/main.py
+++ project/backend/fastapi/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from app.api.chat import router as chat_router
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Chatbot V2")
 
 app.include_router(chat_router, prefix="/api", tags=["Chat"])
 
+
+instrumentator = Instrumentator()
+instrumentator.instrument(app).expose(app)
